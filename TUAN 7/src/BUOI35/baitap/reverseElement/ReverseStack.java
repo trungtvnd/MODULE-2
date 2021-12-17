89fe463efe38fa2845d655b89dbd06b8f5712b2d
trungtvnd <tranviettrung20011992@gmail.com>
2021-12-17 14:14:57 +0700

    add file

---
+++ TUAN 7/src/BUOI35/baitap/reverseElement/ReverseStack.java
@@ -0,0 +1,36 @@
+package BUOI35.baitap.reverseElement;
+
+import com.sun.org.apache.bcel.internal.classfile.StackMap;
+
+import java.util.*;
+
+public class ReverseStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        Stack <String> stackList = new Stack<>();
+//        System.out.println("Nhập vào một chuỗi");
+//        String s = sc.nextLine();
+//        for (int i = 0; i < s.length(); i++) {
+//            stackList.push(s.charAt(i)+"");
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            System.out.print(stackList.pop());
+//        }
+
+
+        Stack <Integer> integerStackList = new Stack<>();
+        int[] intList = new int[5];
+        for (int i = 0; i < intList.length; i++) {
+            System.out.println("Nhập phần tủ thứ" + (i+1));
+            intList[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(intList));
+        for (int i = 0; i < intList.length; i++) {
+            integerStackList.push(intList[i]);
+        }
+        for (int i = 0; i < intList.length; i++) {
+            System.out.print(integerStackList.pop()+",");
+        }
+    }
+
+}
